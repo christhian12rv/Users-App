29af3e255c0db0f8b65833109acece845543edb7
Christhian Rezende Vieira <christhian11rv@hotmail.com>
2022-01-05 19:37:17 -0300

    Remove Navigate from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 
 import Table from './components/Table';
